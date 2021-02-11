a8bd7264a3f830576cdcf3ec16baf039b64d85be
Claudio Domingues <claudio.domingues68@hotmail.com>
2021-02-11 00:49:06 -0300

    Adicionado Scanner

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		Scanner sc = new Scanner(System.in);
 	}
 
 }
